29b55a9f5380f2a77f955c57cb54f97489d92605
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-04-09 21:01:19 -0500

    Updated HTML & JS
    change units to imperial
    added api integration and functions to display city in h1 & current temp in degree

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,24 @@
+function updateWeather(response) {
+  console.log(response.data.temperature.current);
+  let degree = document.querySelector("#degree");
+  let h1 = document.querySelector("#city");
+  h1.innerHTML = response.data.city;
+
+  degree.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "f2784deoc3ec5b8af02864390dt6ce37";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function hanldeSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-bar");
-  let h1 = document.querySelector("#city");
-  h1.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", hanldeSearchSubmit);
+searchCity("Paris");
