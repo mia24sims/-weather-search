0cc50e0c5747ff7810019894d4a9e2e328e29e15
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-04-10 17:46:40 -0500

    Removed html forecast and injected it via javascript
    used forEach loop instead of having code repeating for multiple days

--- src/index.jsx
+++ src/index.jsx
@@ -51,6 +51,30 @@ function hanldeSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `    <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">⛅</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>15&deg;</strong>
+              </div>
+              <div class="weather-forecast-temperature">19&deg;</div>
+            </div>
+          </div>
+          `;
+
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+  });
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", hanldeSearchSubmit);
 searchCity("Paris");
+displayForecast();
